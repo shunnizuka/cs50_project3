15212721bd2c40231e0baa94b3d8820524a8cfa8
shunnizuka <e0201222@u.nus.edu>
2020-09-19 22:12:09 +0800

    Implement view for content of the email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-content').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields and alert
@@ -26,6 +27,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-content').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${
@@ -35,7 +37,9 @@ function load_mailbox(mailbox) {
   fetch_email(mailbox);
 }
 
-// Call /emails/<mailbox> API to fetch emails
+/**
+ * Calls /emails/<mailbox> API to fetch emails and generate email to display
+ */
 function fetch_email(mailbox) {
   fetch(`/emails/${mailbox}`)
     .then(response => response.json())
@@ -56,9 +60,15 @@ function fetch_email(mailbox) {
     });
 }
 
+/**
+ * Create email element to be displayed in the mail-box
+ */
 function create_email(email, mailbox) {
-  const emailRow = document.createElement('div');
-  emailRow.className = 'row';
+  const emailRow = document.createElement('button');
+  emailRow.addEventListener('click', () => {
+    view_email(email.id, mailbox);
+  });
+  emailRow.className = 'emailRow';
   if (email.read && mailbox === 'inbox') {
     // if email is read, display as gray
     emailRow.style.backgroundColor = 'lightgray';
@@ -66,13 +76,15 @@ function create_email(email, mailbox) {
 
   const emailColSender = document.createElement('div');
   emailColSender.className = 'col-3';
-  if(mailbox === 'sent') {
+  if (mailbox === 'sent') {
+    // Show recipients instead of sender in sent mail-box
     var recipientsString = 'To: ';
     email.recipients.forEach(recipient => {
-      recipientsString += recipient + ', '
-    })
+      recipientsString += recipient + ', ';
+    });
     emailColSender.innerHTML = recipientsString.substring(0, recipientsString.length - 2); // to remove the last ', '
   } else {
+    // Show sender in inbox
     emailColSender.innerHTML = email.sender;
   }
 
@@ -86,4 +98,48 @@ function create_email(email, mailbox) {
 
   emailRow.append(emailColSender, emailColContent, emailColTime);
   document.querySelector('#emails-view').append(emailRow);
+}
+
+/**
+ * This function is called when the email element in the mail-box is clicked
+ * Displays the content of the email
+ */
+function view_email(email_id, mailbox) {
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Display the view to show email content
+  const emailContent = document.querySelector('#email-content');
+  emailContent.style.display = 'block';
+
+  // Mark email as read
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+  });
+
+  // get the content of the email to be displayed
+  fetch(`/emails/${email_id}`)
+    .then(response => response.json())
+    .then(email => {
+      // Print email
+      console.log(email);
+      // fill up the content of the view
+      emailContent.querySelector('#sender-field').innerHTML =
+        '<strong>From: </strong>' + email.sender;
+
+      var recipientsString = '';
+      email.recipients.forEach(recipient => {
+        recipientsString += recipient + ', ';
+      });
+      emailContent.querySelector('#recipients-field').innerHTML =
+        '<strong>To: </strong>' + recipientsString.substring(0, recipientsString.length - 2);
+      emailContent.querySelector('#subject-field').innerHTML =
+        '<strong>Subject: </strong>' + email.subject;
+      emailContent.querySelector('#time-field').innerHTML =
+        '<strong>Timestamp: </strong>' + email.timestamp;
+      emailContent.querySelector('#body-field').innerHTML = email.body;
+    });
 }
